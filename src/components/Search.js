b762b4490ddcf64ed18e88803578419e604ee49b
Krystle Lee <l33.krys@gmail.com>
2023-09-05 20:04:35 -0700

    search, filter, exclude used

---
+++ src/components/Search.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Search( { search, setSearch, selectedCategory, setSelectedCategory, showNewAndUsed, setShowNewAndUsed }) {
+
+    return (
+        <div>
+            <div>
+                <input type="text" id="search-bar" placeholder="Search for questions" className="prompt" value={search} onChange={(e) => setSearch(e.target.value)}/>
+            </div>
+            <div>
+                <label>
+                    <strong>Filter by Category:</strong>
+                    <select name="category" id="filter-category" onChange={(e) => setSelectedCategory(e.target.value)}>
+                        <option value="All">All</option>
+                        <option value="recreation">Recreation</option>
+                        <option value="work">Work</option>
+                        <option value="miscellaneous">Miscellaneous</option>
+                    </select>
+                </label>
+            </div>
+            <div>
+                <label>
+                    <strong>Exclude Used Questions:</strong>
+                    <input type="checkbox" id="new-or-repeat" name="completed" onClick={(e) => setShowNewAndUsed(e.target.checked)}/>
+                </label>
+            </div>
+        </div>
+      );
+}
+
+export default Search;
