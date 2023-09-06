58674dad9c8be8b88e1f772f3bb449c24e558bcf
Jia-Jia Zhu <dodge_waking_0b@icloud.com>
2023-09-06 11:05:27 -0700

    question card, list, and container code

--- src/components/QuestionsContainer.js
+++ src/components/QuestionsContainer.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionsList from "./QuestionsList";
 
 function QuestionsContainer() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/questions")
+      .then((r) => r.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
   return (
     <div>
       <h2>Today's Question</h2>
-      {/* add components - QuestionList, Sort, Search, CategoryFilter*/}
+      <QuestionsList questions={questions} />
     </div>
   );
 }
