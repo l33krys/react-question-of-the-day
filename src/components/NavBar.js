05b71db45806eaadae0ed1d9cd70fea7786fae33
Madison <mlinkenmeyer@gmail.com>
2023-09-07 11:44:08 -0700

    nav-bar-styled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,44 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import { AppBar, Toolbar, Box } from "@mui/material";
 const NavBar = () => {
   return (
-    <div>
-      <nav>
-        <h1>Logo</h1>
-        <ul>
-          <li>
-            <NavLink to="/" exact activeClassName="active-link">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/add-question" activeClassName="active-link">
-              Add Question
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/questions" activeClassName="active-link">
-              Questions
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard" activeClassName="active-link">
-              Dashboard
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" className=".app-bar">
+        <Toolbar className=".tool-bar">
+          <h1>Logo</h1>
+          <NavLink
+            to="/"
+            exact
+            className="navbar-link"
+            activeClassName="active-link"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/add-question"
+            className="navbar-link"
+            activeClassName="active-link"
+          >
+            Add Question
+          </NavLink>
+          <NavLink
+            to="/questions"
+            className="navbar-link"
+            activeClassName="active-link"
+          >
+            Questions
+          </NavLink>
+          <NavLink
+            to="/dashboard"
+            className="navbar-link"
+            activeClassName="active-link"
+          >
+            Dashboard
+          </NavLink>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
